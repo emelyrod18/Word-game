fc47de319b9ee7850439ab7fb6f77c9b71e0984a
emelyrod18 <emelyrodriguez1810@gmail.com>
2026-02-25 21:16:11 -0400

    word list

--- word.py
+++ word.py
@@ -1,1 +1,8 @@
-print("hi")
+import random
+
+words = [
+    "bubblegum", "undertale", "roblox", "butterfly", "yourself", "python", "cheese", "sourdough"
+    "nine", "clock", "flan", "alliteration", "mickey", "fortnite", "hacker"
+]
+
+random.shuffle(words)
